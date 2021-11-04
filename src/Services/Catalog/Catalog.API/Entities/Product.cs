62f0aa3514397d669ffa163d96efbff04151d031
Costin Somodea <costin.somodea@yahoo.com>
2021-11-04 16:25:07 +0200

    Catalog.API service

---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public record Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; init; }
+        [BsonElement("Name")]
+        public string Name { get; init; }
+        public string Category { get; init; }
+        public string Summary { get; init; }
+        public string Description { get; init; }
+        public string ImageFile { get; init; }
+        public decimal Price { get; init; }
+    }
+}
