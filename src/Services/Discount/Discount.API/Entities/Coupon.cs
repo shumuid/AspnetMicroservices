93f0d9bb61083453b7c7e3bea35f92383d3be654
Costin Somodea <costin.somodea@yahoo.com>
2021-11-09 12:43:51 +0200

    discount api

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    public record Coupon
+    {
+        public int Id { get; init; }
+        public string ProductName { get; init; }
+        public string Description { get; init; }
+        public int Amount { get; init; }
+    }
+}
